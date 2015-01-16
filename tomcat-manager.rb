31d61665c79807aa25918b9b507dc632b01713c5
Jess Thrysoee <jet@ccieurope.com>
2015-01-16 12:26:32 +0100

    tomcat-manager 1.1.0

--- tomcat-manager.rb
+++ tomcat-manager.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class TomcatManager < Formula
    homepage "https://github.com/JessThrysoee/tomcat-manager"
-   url "https://github.com/JessThrysoee/tomcat-manager.git", :tag => "1.0.0"
+   url "https://github.com/JessThrysoee/tomcat-manager.git", :tag => "1.1.0"
 
    def install
       bin.install 'tomcat-manager'
