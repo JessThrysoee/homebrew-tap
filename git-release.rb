2c0553e86be81659f3ef19a9bfdca06fcc7bff45
Jess Thrysoee <jess@thrysoee.dk>
2015-01-26 20:58:49 +0100

    git-release 1.1.0

--- git-release.rb
+++ git-release.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GitRelease < Formula
    homepage "https://github.com/JessThrysoee/git-release/"
-   url "https://github.com/JessThrysoee/git-release.git", :tag => "1.0.0"
+   url "https://github.com/JessThrysoee/git-release.git", :tag => "1.1.0"
 
    def install
       bin.install 'git-release'
