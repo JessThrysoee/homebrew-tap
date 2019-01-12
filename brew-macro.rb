fadf25626588609e6acad735dd50e8c8b50e0662
Jess Thrysoee <jess@thrysoee.dk>
2019-01-12 18:01:43 +0100

    brew-macro 1.1.2

--- brew-macro.rb
+++ brew-macro.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class BrewMacro < Formula
    homepage "https://gist.github.com/JessThrysoee/311ea8a0728c9556b061"
-   url "https://gist.github.com/JessThrysoee/311ea8a0728c9556b061.git", :tag => "1.1.1"
+   url "https://gist.github.com/JessThrysoee/311ea8a0728c9556b061.git", :tag => "1.1.2"
 
    def install
       bin.install 'brew-macro'
